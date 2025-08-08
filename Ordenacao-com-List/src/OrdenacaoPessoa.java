31ce6e6f79e9df27018e384c96b1cce9ffde4e5c
Zev07 <jose.victor.zev7dev@gmail.com>
2025-08-08 15:32:07 -0300

    feat: Resolução dos exercicios de Pesquisa e Ordenação em List

---
+++ Ordenacao-com-List/src/OrdenacaoPessoa.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class OrdenacaoPessoa {
+    private List<Pessoa> pessoas;
+
+    public List<Pessoa> pessoaList;
+
+    public OrdenacaoPessoa () {
+        this.pessoaList = new ArrayList<>();
+    }
+
+    public void adicionarPessoa(String nome, int idade, double altura) {
+        pessoaList.add(new Pessoa(nome, idade, altura));
+    }
+
+    public List<Pessoa> ordenacaoPorIdade (){
+        List<Pessoa> pessoaPorIdade = new ArrayList<>(pessoaList);
+        Collections.sort(pessoaPorIdade);
+        return pessoaPorIdade;
+    }
+
+    public List<Pessoa> ordenacaoPorAltura() {
+        List<Pessoa> pessoaPorAltura = new ArrayList<>(pessoaList);
+        Collections.sort(pessoaPorAltura, new ComparatorPorAltura());
+        return pessoaPorAltura;
+    }
+
+}
+
---
+++ Pesquisa-em-List/src/CatalogoLivros.java
@@ -0,0 +1,52 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CatalogoLivros {
+    private List<Livro> livroList;
+
+    public CatalogoLivros() {
+        this.livroList = new ArrayList<>();
+    }
+
+    public void adicionarLivro(String titulo, String autor, int anoPublicacao) {
+        livroList.add(new Livro(titulo, autor, anoPublicacao));
+    }
+
+    public List<Livro> pesquisarPorAutor(String autor) {
+        List<Livro> livroPorAutor = new ArrayList<>();
+        if(!livroList.isEmpty()) {
+            for(Livro l : livroList) {
+                if (l.getAutor().equalsIgnoreCase(autor)) {
+                    livroPorAutor.add(l);
+                }
+            }
+        }
+        return livroPorAutor;
+    }
+
+    public List<Livro> pesquisarPorIntervaloAnos(int anoInicial, int anoFinal) {
+        List<Livro> livrosNoIntervalo = new ArrayList<>();
+        if(!livroList.isEmpty()) {
+            for(Livro l : livroList) {
+                if (l.getAnoPublicacao() >= anoInicial && l.getAnoPublicacao() <= anoFinal) {
+                    livrosNoIntervalo.add(l);
+                }
+            }
+        }
+        return livrosNoIntervalo;
+    }
+
+    public Livro pesquisarPorTitulo(String titulo) {
+        Livro livroPorTitulo = null;
+        if(!livroList.isEmpty()) {
+            for(Livro l : livroList) {
+                if (l.getTitulo().equalsIgnoreCase(titulo)) {
+                    livroPorTitulo = l;
+                    break;
+                }
+            }
+        }
+        return livroPorTitulo;
+    }
+}
